2780db03dfd17d8cff4c9154ab0d46beb28d858d
Fábio  Maiorano <fsmaiorano@MacBook-Air-de-Fabio.local>
2018-05-31 11:50:08 -0300

    add: signin and signup methods

--- backend/app/controllers/authController.js
+++ backend/app/controllers/authController.js
@@ -4,7 +4,35 @@ const User = mongoose.model("User");
 
 module.exports = {
     async signup(req, res, next) {
-        console.log("passei");
-        return res.send("teste");
+        const { email, password } = req.body;
+
+        if (await User.findOne({ $or: [{ email }, { password }] })) {
+            res.status(400).json({ error: "User already exists" });
+        }
+
+        const user = await User.create(req.body);
+
+        return res.json({ user, token: user.generateToken() });
+    },
+
+    async signin(req, res, next) {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            res.status(400).json({ error: "User not found" });
+        }
+
+        if (!(await user.compareHash(password))) {
+            return res.status(400).json({ error: "Invalid password" });
+        }
+
+        req.user = user;
+
+        return res.json({
+            user,
+            token: user.generateToken()
+        });
     }
 };
--- backend/app/routes.js
+++ backend/app/routes.js
@@ -4,7 +4,13 @@ const requireDir = require("require-dir");
 const routes = express.Router();
 const controllers = requireDir("./controllers");
 
+const authMiddleware = require("./middlewares/auth");
+
 // Authentication
 routes.post("/signup", controllers.authController.signup);
+routes.post("/signin", controllers.authController.signin);
+
+// Auth Middleware
+routes.use(authMiddleware);
 
 module.exports = routes;
