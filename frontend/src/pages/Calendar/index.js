4205a3be4b3a5aa8431ac33f56409089af1050b6
Fábio <fsmaiorano@gmail.com>
2018-06-11 21:00:34 -0300

    add: calendar

--- frontend/src/pages/Calendar/index.js
+++ frontend/src/pages/Calendar/index.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import ReactCalendar from 'react-calendar';
+
+import { ContainerCalendar } from './styles';
 
 class Calendar extends Component {
+  state = {
+    date: new Date(),
+  };
+
+  onChange = date => this.setState({ date });
+
   render() {
-    return <h1>Calendar</h1>;
+    return (
+      <section>
+        <h1>Schedule</h1>
+        <ContainerCalendar>
+          <ReactCalendar onChange={this.onChange} value={this.state.date} />
+        </ContainerCalendar>
+      </section>
+    );
   }
 }
 
---
+++ frontend/src/pages/Calendar/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: 0 auto;
+  transform: translate(-50%, -50%);
+`;
+
+export const ContainerCalendar = styled.div`
+  margin: 0 auto;
+  width: 20%;
+`;
