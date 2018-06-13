31cf0184a1fb028da1f792970b30cf172d97c363
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-06-13 14:29:13 -0300

    update: add new schedule

--- backend/app/controllers/calendarController.js
+++ backend/app/controllers/calendarController.js
@@ -5,6 +5,42 @@ const Calendar = mongoose.model("Calendar");
 
 module.exports = {
     async add(req, res, next) {
-        const x = req.body;
+        const { title, location, date, hour } = req.body;
+
+        let user = await User.findById(req.userId);
+
+        if (!user) {
+            return res.json({
+                success: false,
+                msg: "Token invalid",
+                result: null
+            });
+        }
+
+        let schedule = {
+            title,
+            location,
+            date,
+            hour,
+            user
+        }
+
+        let newSchedule = await Calendar.create(schedule);
+
+        if(newSchedule.errors) {
+            return res.json({
+                success: false,
+                msg: "Outch!",
+                result: newSchedule.errors
+            });
+        }
+
+        return res.json({
+            success: true,
+            msg: "Schedule created with success",
+            result: newSchedule
+        });
+
+
     }
 };
