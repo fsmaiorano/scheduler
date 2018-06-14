0ca4d5f5ff4889730c6a3a75ca8525e6a50b84bc
Fábio <fsmaiorano@gmail.com>
2018-06-13 21:11:41 -0300

    update: time

--- frontend/src/pages/Calendar/index.js
+++ frontend/src/pages/Calendar/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactCalendar from 'react-calendar';
+import TimePicker from 'react-time-picker';
 
 import { add } from '../../services/calendar';
 
@@ -8,26 +9,38 @@ import { ContainerCalendar } from './styles';
 class Calendar extends Component {
     state = {
       date: new Date(),
+      time: new Date().toLocaleTimeString(),
       selectedDate: new Date().toLocaleDateString(),
+      selectedTime: new Date().toLocaleTimeString(),
       title: '',
       location: '',
     };
 
-    onChange = (date) => {
+    onChangeDate = (date) => {
       this.setState({ selectedDate: date.toLocaleDateString() });
+    };
+
+    onChangeTime = (time) => {
+      this.setState({ selectedTime: time });
     };
 
     addEvent = async (e) => {
       e.preventDefault();
-      const { title, location, selectedDate } = this.state;
-      const newEvent = {
+      const {
         title,
         location,
         selectedDate,
+        selectedTime,
+      } = this.state;
+
+      const newEvent = {
+        title,
+        location,
+        date: selectedDate,
+        hour: selectedTime,
       };
 
       const { data } = await add(newEvent);
-      debugger;
     }
 
     render() {
@@ -36,9 +49,13 @@ class Calendar extends Component {
           <h1>Schedule</h1>
           <ContainerCalendar>
             <ReactCalendar
-              onChange={this.onChange}
+              onChange={this.onChangeDate}
               value={this.state.date}
               minDate={new Date()}
+            />
+            <TimePicker
+              onChange={this.onChangeTime}
+              value={this.state.time}
             />
             <form onSubmit={this.addEvent}>
               <input type="text" placeholder="Nome do evento" onChange={e => this.setState({ title: e.target.value })} />
