94346a6b450db4fdcab5f55dda0aa906304404d1
Fábio <fsmaiorano@gmail.com>
2018-06-24 15:05:46 -0300

    update: api call for signin

--- frontend-react/src/pages/Authentication/index.js
+++ frontend-react/src/pages/Authentication/index.js
@@ -1,18 +1,68 @@
 import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+
+import { signin } from "../../services/authentication"
 
 class Authentication extends Component {
+    static propTypes = {
+        history: PropTypes.func.isRequired
+    };
+
+    state = {
+        email: "",
+        password: "",
+        validations: {
+            isLoading: false,
+            isError: false
+        }
+    };
+
+    doLogin = async e => {
+        try {
+            e.preventDefault();
+            this.setState({ isLoading: true });
+
+            const { email, password } = this.state;
+
+            const { data } = await signin({
+                email: email,
+                passord: password
+            });
+
+            if (data.success) {
+                sessionStorage.setItem(
+                    "user",
+                    JSON.stringify(data.result.user)
+                );
+                sessionStorage.setItem("token", `Bearer ${data.result.token}`);
+                // this.props.history.push("/calendar");
+            } else {
+                this.setState({ isLoading: false });
+            }
+            this.setState({ isLoading: false });
+        } catch (error) {
+            this.setState({ isLoading: false });
+        }
+    };
+
     render() {
         return (
             <section className="authentication">
                 <div className="bg-video">
                     <video className="bg-video__content" autoPlay muted loop>
-                        <source src="/assets/videos/Office-Day.mp4" type="video/mp4" />
-                        <source src="assets/videos/Office-Day.webm" type="video/webm" />
+                        <source
+                            src="/assets/videos/Office-Day.mp4"
+                            type="video/mp4"
+                        />
+                        <source
+                            src="assets/videos/Office-Day.webm"
+                            type="video/webm"
+                        />
                         Your browser is not supported!
                     </video>
                 </div>
 
-                <form className="form">
+                <form className="form" onSubmit={this.doLogin}>
                     <div className="u-center-text u-margin-bottom-small">
                         <h2 className="heading-secondary">Scheduler</h2>
                     </div>
@@ -24,6 +74,9 @@ class Authentication extends Component {
                             placeholder="E-mail"
                             required
                             autoComplete="off"
+                            onChange={e =>
+                                this.setState({ email: e.target.value })
+                            }
                         />
                         <label htmlFor="email" className="form__label">
                             E-mail
@@ -37,13 +90,18 @@ class Authentication extends Component {
                             placeholder="Senha"
                             required
                             autoComplete="off"
+                            onChange={e =>
+                                this.setState({ password: e.target.value })
+                            }
                         />
                         <label htmlFor="password" className="form__label">
                             Senha
                         </label>
                     </div>
                     <div className="form__group u-float-right">
-                        <button className="btn btn--primary">Entrar</button>
+                        <button type="submit" className="btn btn--primary">
+                            Entrar
+                        </button>
                     </div>
                 </form>
             </section>
