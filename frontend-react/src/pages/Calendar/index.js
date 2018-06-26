ff5ae1c82cecb6abb8e987bd6553fc8d63fd133b
Fábio <fsmaiorano@gmail.com>
2018-06-26 18:07:20 -0300

    update: refactory

--- frontend-react/src/pages/Calendar/index.js
+++ frontend-react/src/pages/Calendar/index.js
@@ -4,7 +4,13 @@ class Calendar extends Component {
 render() {
     return(
         <section className="section-calendar">
-
+            <header className="header">
+                <nav className="header__nav">
+                    <div className="header__box">
+                        <span>User</span>
+                    </div>
+                </nav>
+            </header>
         </section>
     )
 }
